065b0f4a2f39c8ff86dce7dc4c916c0b199702e2
Denis Volok <denis.volok@pm.me>
2024-06-02 21:40:56 +0000

    feat: test keyboard buttons

--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,17 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
 import { ENV } from "./utils/env/env";
 
 export const getBot = () => {
-  // Create an instance of the `Bot` class and pass your bot token to it.
   const bot = new Bot(ENV.BOT_TOKEN);
-  // You can now register listeners on your bot object `bot`.
-  // grammY will call the listeners when users send messages to your bot.
 
-  // Handle the /start command.
   bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-  // Handle other messages.
-  bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-  // Now that you specified how to handle messages, you can start your bot.
-  // This will connect to the Telegram servers and wait for messages.
+  const kb = new InlineKeyboard().url("Click me!", "https://example.com");
+  bot.on("message", (ctx) => {
+    ctx.reply("Got another message!", {
+      reply_markup: kb,
+    });
+  });
 
   return bot;
 };
